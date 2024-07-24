c2bc48969c49cde84d54a9b7e262791559ed68f2
Alireza <169320894+adnx1994@users.noreply.github.com>
2024-07-24 07:25:25 +0330

    JavaScript section separation

---
+++ z1/slider.js
@@ -0,0 +1,23 @@
+
+function write() {
+    let par1 = document.getElementById("name").innerHTML = "my name is Ali";
+
+    let par2 = document.getElementById("tily").innerHTML = "my skill is programing";
+
+}
+
+
+btn.addEventListener("click", write);
+
+
+
+
+dokmeh.addEventListener("click",function(){
+    let par1 = document.getElementById("name");  
+    par1.classList.add("rang");
+
+})
+
+
+
+
